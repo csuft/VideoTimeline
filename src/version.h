fa4af29d730f9ff47482c347e3f529aaee926df0
zzkhnb <zhangzhongke@arashivision.com>
2018-01-22 17:08:43 +0800

    create project

---
+++ src/version.h
@@ -0,0 +1,9 @@
+#ifndef __VERSION_H__
+#define __VERSION_H__
+
+// version variable that will be substituted by cmake
+// This shows an example using the $ variable type
+const char* verid = "Product: TimelineRuler Commit: ";
+const char* vernum = "Version: 0.0.1";
+
+#endif
