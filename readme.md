b5ee35ae2a4b520df863d1a467c8955b48cf1c58
zzkhnb <zhangzhongke@arashivision.com>
2018-01-22 17:15:02 +0800

    update readme

---
+++ readme.md
@@ -0,0 +1,13 @@
+# VideoTimeline
+
+A timeline widget for video editing software.
+
+## Build
+- Qt 5.8 or later
+- CMake 3.8 or higher
+
+## Snapshot
+coming soon
+
+## License
+[MIT]()
--- src/version.h
+++ src/version.h
@@ -3,7 +3,7 @@
 
 // version variable that will be substituted by cmake
 // This shows an example using the $ variable type
-const char* verid = "Product: TimelineRuler Commit: ";
+const char* verid = "Product: TimelineRuler Commit: fa4af29d730f9ff47482c347e3f529aaee926df0";
 const char* vernum = "Version: 0.0.1";
 
 #endif
