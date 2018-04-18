bdee78fc6a4a0757569009584ba933a4f69cd653
zzkhnb <zhangzhongke@arashivision.com>
2018-04-18 17:19:08 +0800

    update

--- src/version.h
+++ src/version.h
@@ -3,7 +3,7 @@
 
 // version variable that will be substituted by cmake
 // This shows an example using the $ variable type
-const char* verid = "Product: TimelineRuler Commit: ca178ab9ed8915b577204827d6bbba3c2d5eadd1";
+const char* verid = "Product: TimelineRuler Commit: 2cf3fb6785d55e33c3d0975bd2d65e47c54f0716";
 const char* vernum = "Version: 0.0.1";
 
 #endif
